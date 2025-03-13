ed37393716d7d77416c0399f190a13c04bb7eec0
rafaelomodei <rafael.omodei@outlook.com>
2025-03-12 21:23:27 -0300

    feat: created component transactionTable

---
+++ src/shared/components/molecules/TransactionTable/TransactionTable.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { Badge } from '@/shared/components/ui/badge';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/shared/components/ui/table';
+import { TransactionDTO } from '@/shared/interface/transaction/transaction.dto';
+import { formatDate } from '@/shared/utils/date';
+
+interface TransactionTableProps {
+  transactions: TransactionDTO[] | undefined;
+  isLoading: boolean;
+  error: unknown;
+}
+
+const TransactionTable = ({
+  transactions,
+  isLoading,
+  error,
+}: TransactionTableProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Nome</TableHead>
+          <TableHead className='hidden sm:table-cell'>Tipo</TableHead>
+          <TableHead className='hidden sm:table-cell'>Meio</TableHead>
+          <TableHead className='hidden sm:table-cell'>Status</TableHead>
+          <TableHead className='hidden md:table-cell'>Data da compra</TableHead>
+          <TableHead className='text-right'>Valor</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {isLoading ? (
+          <TableRow>
+            <TableCell colSpan={6} className='text-center'>
+              Carregando...
+            </TableCell>
+          </TableRow>
+        ) : error ? (
+          <TableRow>
+            <TableCell colSpan={6} className='text-center text-red-500'>
+              Erro ao carregar as transações.
+            </TableCell>
+          </TableRow>
+        ) : transactions?.length === 0 ? (
+          <TableRow>
+            <TableCell
+              colSpan={6}
+              className='text-center text-muted-foreground'
+            >
+              Nenhuma transação encontrada.
+            </TableCell>
+          </TableRow>
+        ) : (
+          transactions?.map((transaction, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                <div className='font-medium'>{transaction.label}</div>
+              </TableCell>
+              <TableCell className='hidden sm:table-cell'>
+                {transaction.type}
+              </TableCell>
+              <TableCell className='hidden sm:table-cell'>
+                {transaction.bank}
+              </TableCell>
+              <TableCell className='hidden sm:table-cell'>
+                <Badge variant='secondary'>{transaction.paymentStatus}</Badge>
+              </TableCell>
+              <TableCell className='hidden md:table-cell'>
+                {formatDate(transaction.paymentDate)}
+              </TableCell>
+              <TableCell className='text-right'>{`R$ ${transaction.value.toFixed(
+                2
+              )}`}</TableCell>
+            </TableRow>
+          ))
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export { TransactionTable };
